3a8f86ef28b969a9967382edd49d22065ca9ee6d
FedeConciencia <federicosabatini@gmail.com>
2023-03-12 18:46:27 -0300

    Se corrigen problemas con servicios

--- BackEnd/portfolio/portfolio/src/main/java/com/proyecto/portfolio/services/DetalleProSkillService.java
+++ BackEnd/portfolio/portfolio/src/main/java/com/proyecto/portfolio/services/DetalleProSkillService.java
@@ -7,8 +7,9 @@ import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class DetalleProSkillService implements BaseService<DetalleProSkill> {
     
     @Autowired
